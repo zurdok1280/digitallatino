f5b0934af5e15ae0242fc540f8102415f9171ec3
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2025-11-04 08:05:19 -0600

    Update Campaign.tsx

--- src/pages/Campaign.tsx
+++ src/pages/Campaign.tsx
@@ -1,19 +1,44 @@
 import React, { useEffect } from 'react';
+import { useAuth } from '@/hooks/useAuth';
+
 
 const SongToolsWidget = () => {
+  const { token } = useAuth();
+  const baseUrl = "https://widgets.songtools.io/v1/CampaignTestX?app-key=21B0C76233F14B2CB724FE0134A0F167&autodetect=1";
+  const iframeSrc = token ? `${baseUrl}&jwt=${token}` : baseUrl;
   useEffect(() => {
     // Configurar la comunicación con el iframe
     const isMobile = false;
     const eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
     const eventer = window[eventMethod];
     const messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+    
 
     const processParentEvent = (e) => {
       console.log('Evento recibido del iframe:', e);
     };
 
     eventer(messageEvent, processParentEvent);
-
+    const loadScript = (src: string, integrity?: string, crossorigin?: string) => {
+    return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.type = 'text/javascript';
+    if (integrity) script.integrity = integrity;
+    if (crossorigin) script.crossOrigin = crossorigin;
+   script.onload = resolve;
+   script.onerror = reject;
+   document.head.appendChild(script);
+  });
+ };
+   loadScript(
+    'https://digitallatino.songtools.io/js/wixgetcontent.js?v=1738787616'
+    );
+   loadScript(
+    'https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6706d724bb0e4c6b5a5c849b',
+    'sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=',
+    'anonymous'
+   );
     // Cleanup function
     return () => {
       if (eventMethod === "addEventListener") {
@@ -33,7 +58,7 @@ const SongToolsWidget = () => {
         scrolling="no"
         width="100%"
         height="1200"
-        src="https://widgets.songtools.io/v1/CampaignTestX?app-key=21B0C76233F14B2CB724FE0134A0F167&autodetect=1"
+        src={iframeSrc}
         style={{
           backgroundColor: 'transparent',
           height: '900px',
